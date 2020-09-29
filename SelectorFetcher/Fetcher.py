5174eb00feefd724706c67151c8b989faf814c59
Vinayak Mathur <vinayakrulez@gmail.com>
2020-09-28 21:01:06 -0400

    Local Commit for ver1.0

---
+++ SelectorFetcher/Fetcher.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib as plt
+
+class Fetcher:
+    # Gets the information about each of the stock sent to it.
+    # Like 5 or 10 year history, description, name, etc.
+
+    def screen(self):
+        # Screens and finds the stocks with the given criteria in a list format.
+        pass
