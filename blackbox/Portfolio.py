a8ae1014f125403505f1e997530ad36b521526ac
Vinayak Mathur <vinayakrulez@gmail.com>
2020-09-28 20:46:57 -0400

    Local Commit for ver1.0

---
+++ blackbox/Portfolio.py
@@ -0,0 +1,8 @@
+import json
+
+
+class Portfolio:
+    # Most important class.
+    # Takes JSON from the UI with answers to the questions and sends fetch requests to the Stock Selector.
+    def __init__(self):
+        pass
