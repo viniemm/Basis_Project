bdb05b0c00d5ea7b627c601f4e7fbe716a6dafe5
Vinayak Mathur <vinayakrulez@gmail.com>
2020-10-29 06:54:28 -0400

    DTable ready for demo1

--- databases/DTable.py
+++ databases/DTable.py
@@ -4,27 +4,46 @@ import matplotlib as plt
 import yfinance as yf
 
 
-def get(ticker) -> dict:
-    stock = yf.Ticker(ticker)
-    pass
+class DTable:
+    def __init__(self):
+        self.db = pd.read_csv("BasisDB.csv")
+        self.col = self.db.columns
 
+    def get_single(self, ticker: str) -> dict:
+        return self.db.loc[self.db["symbol"] == ticker.upper()].to_dict(orient="records")
 
-def add(new_ticker, new_tags):
-    pass
+    def get(self, tickers: str) -> list:
+        tickers = tickers.strip().split()
+        stocks = list()
+        for tick in tickers:
+            stocks.append(self.get_single(tick))
+        return stocks
 
+    def add_single(self, new_ticker):
+        stk = yf.Ticker(new_ticker).info
+        row = list()
+        for field in self.col:
+            row.append(stk[field])
+        df = dict(zip(self.col, row))
+        self.db = self.db.append(df, ignore_index=True)
 
-def remove(old_ticker):
+    def add(self, tickers: str):
+        tickers = tickers.strip().split()
+        for ticker in tickers:
+            self.add_single(ticker)
 
-    pass
+    def remove_single(self, old_ticker: str):
+        self.db.drop(self.db[self.col[0]] == old_ticker)
 
+    def remove(self, tickers: str):
+        tickers = tickers.strip().split()
+        for ticker in tickers:
+            self.remove_single(ticker)
 
-def replace(old_ticker, new_ticker, new_tags):
-    pass
+    def replace(self, old_tickers: str, new_tickers: str):
+        self.remove(old_tickers)
+        self.add(new_tickers)
 
-
-def modify(old_ticker: str, new_tags: list):
-    pass
-
-
-def get_tags(old_ticker):
-    pass
+    def done(self):
+        df = self.db.drop_duplicates()
+        df.to_csv("BasisDB.csv", index=False)
---
+++ databases/tester.py
@@ -0,0 +1,5 @@
+from DTable import DTable
+
+dt = DTable()
+dt.add("msft amzn aapl")
+dt.done()
